654f145331b22bab1599efadbbdfb84b46ab33a0
abonaser87 <abonaser.87@gmail.com>
2015-12-20 15:23:11 +0300

    Final python format is done and written to file

--- src/ckts.java
+++ src/ckts.java
@@ -3,6 +3,8 @@ import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import java.util.List;
 
@@ -21,29 +23,42 @@ public class ckts {
     JButton addline = new JButton("Add Line");
     JButton createPython = new JButton("Create Python");
     GridBagConstraints c = new GridBagConstraints();
-    Map<Integer, Integer> totalParts = new TreeMap<Integer, Integer>();
+    Map<Integer, Integer> totalParts = new TreeMap<>();
     Map<Integer, ArrayList<JTextField>> fromTxt = new TreeMap<>();
     Map<Integer, JCheckBox> dckts = new TreeMap<>();
     Map<Integer, ArrayList> paramters = new TreeMap<>();
-    Map<String, List<Double>> data = new TreeMap<String, List<Double>>();
+    Map<String, List<Double>> data = new TreeMap<>();
+    Map<String, Integer> mva = new TreeMap<>();
     private int line = 1;
 
     public ckts() {
 
-        data.put("ACSR-CONDOR 1x402", Arrays.asList(0.000513,0.002753,0.000627,0.001103,0.005573));
-        data.put("ACSR-CONDOR 2x402",Arrays.asList(0.000257,0.001795,0.000897,0.000981,0.005173));
-        data.put("ACSR-HAWK 1x240",Arrays.asList(0.000855,0.002697,0.000602,0.001265,0.005625));
-        data.put("GTACSR-CONDOR 1x402",Arrays.asList(0.000513,0.002753,0.000627,0.001414,0.006851));
-        data.put("OSTRICH 1x152",Arrays.asList(0.001358,0.002797,0.000580,0.001505,0.005675));
-        data.put("XLPE 1x1000",Arrays.asList(0.000134,0.001483,0.013787,0.000562,0.000581));
-        data.put("XLPE 1x1200",Arrays.asList(0.000103,0.001426,0.015194,0.000461,0.000495));
-        data.put("GTACSR-HAWK 1x240",Arrays.asList(0.000855,0.002697,0.000602,0.001265,0.005625));
-        data.put("H-resistance-SAPS 1x282",Arrays.asList(0.000730,0.002675,0.000607,0.001206,0.005614));
-        data.put("XLPE 1x1600",Arrays.asList(0.000097,0.001406,0.015765,0.000421,0.000520));
-        data.put("XLPE 1x2000",Arrays.asList(0.000077,0.001363,0.017079,0.000335,0.000504));
-        data.put("ACSR-HAWK 1x240",Arrays.asList(0.000427,0.001348,0.001204,0.000633,0.002813));
-        data.put("Condor-Banked 1x402",Arrays.asList(0.000256,0.001376,0.001254,0.000551,0.002786));
-
+        data.put("ACSR-CONDOR 1x402", Arrays.asList(0.0005125115, 0.0027525253, 0.0006271000, 0.0011025023, 0.0055727732));
+        data.put("ACSR-CONDOR 2x402", Arrays.asList(0.0002565427, 0.0017952250, 0.0008966069, 0.0009808310, 0.0051733242));
+        data.put("ACSR-HAWK 1x240", Arrays.asList(0.0008545684, 0.0026968549, 0.0006019419, 0.0012654959, 0.0056250000));
+        data.put("GTACSR-CONDOR 1x402", Arrays.asList(0.0005125115, 0.0027525253, 0.0006271000, 0.0014135675, 0.0068514692));
+        data.put("OSTRICH 1x152", Arrays.asList(0.0013584711, 0.0027967172, 0.0005797332, 0.0015048209, 0.0056749311));
+        data.put("XLPE 1x1000", Arrays.asList(0.0001342975, 0.0014834711, 0.0137870014, 0.0005624426, 0.0005809803));
+        data.put("XLPE 1x1200", Arrays.asList(0.0001033058, 0.0014261938, 0.0151937280, 0.0004612029, 0.0004951217));
+        data.put("GTACSR-HAWK 1x240", Arrays.asList(0.0008545684, 0.0026968549, 0.0006019419, 0.0012654959, 0.0056250000));
+        data.put("H-resistance-SAPS 1x282", Arrays.asList(0.0007300275, 0.0026750459, 0.0006069407, 0.0012058081, 0.0056140955));
+        data.put("XLPE 1x1600", Arrays.asList(0.0000972796, 0.0014063603, 0.0157648638, 0.0004208549, 0.0005195086));
+        data.put("XLPE 1x2000", Arrays.asList(0.0000774793, 0.0013630877, 0.0170786024, 0.0003351942, 0.0005035237));
+        data.put("Banked-HAWK 1x240", Arrays.asList(0.0004272842, 0.0013484275, 0.0012038838, 0.0006327479, 0.0028125000));
+        data.put("Banked-Condor 1x402", Arrays.asList(0.0002562557, 0.0013762626, 0.0012542000, 0.0005512511, 0.0027863866));
+        mva.put("ACSR-CONDOR 1x402", 151);
+        mva.put("ACSR-CONDOR 2x402", 274);
+        mva.put("ACSR-HAWK 1x240", 102);
+        mva.put("GTACSR-CONDOR 1x402", 274);
+        mva.put("OSTRICH 1x152", 77);
+        mva.put("XLPE 1x1000", 187);
+        mva.put("XLPE 1x1200", 209);
+        mva.put("GTACSR-HAWK 1x240", 203);
+        mva.put("H-resistance-SAPS 1x282", 226);
+        mva.put("XLPE 1x1600", 242);
+        mva.put("XLPE 1x2000", 267);
+        mva.put("Banked-HAWK 1x240", 204);
+        mva.put("Banked-Condor 1x402", 302);
 
         frame.setContentPane(rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -65,22 +80,25 @@ public class ckts {
         createPython.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Store the data in 2D String array as PSSE format
+                String[][] finaldata = new String[fromTxt.size()][6];
                 // Get the From and To Text
                 for (Integer key : fromTxt.keySet()) {
-                    for (JTextField jTextField : fromTxt.get(key)) {
-                        System.out.println(jTextField.getText());
-                    }
+                    finaldata[key - 1][0] = "psspy.branch_data(1" + fromTxt.get(key).get(0).getText() + ",1" + fromTxt.get(key).get(1).getText() + ",r\"\"\"1\"\"\",[_i,_i,_i,_i,_i,_i],";
+                    finaldata[key - 1][4] = "psspy.seq_branch_data(1" + fromTxt.get(key).get(0).getText() + ",1" + fromTxt.get(key).get(1).getText() + ",r\"\"\"1\"\"\",";
                 }
                 // Get the Double Circuit check
                 for (Integer key : dckts.keySet()) {
                     System.out.println(dckts.get(key).isSelected());
+                    // use replace with
                 }
                 // Get the parameters and calculate the Length * Type
                 for (Integer key : paramters.keySet()) {
                     // Temporary Arrays to store the data
                     ArrayList<List> para = new ArrayList<>();
-                    ArrayList<Double> km = new ArrayList<Double>();
-                    // The first coulmn of the array is the Length and the second is the Type
+                    ArrayList<Double> km = new ArrayList<>();
+                    ArrayList<Integer> mvaVal = new ArrayList<>();
+                    // The first column of the array is the Length and the second is the Type
                     // This is a for loop to extract each one
                     for (int i = 0; i < paramters.get(key).size(); i++) {
                         if (i % 2 == 0 | i == 0) {
@@ -88,11 +106,20 @@ public class ckts {
                             km.add(Double.valueOf(((JTextField) paramters.get(key).get(i)).getText()));
                         } else {
                             System.out.println(data.get(((JComboBox) paramters.get(key).get(i)).getSelectedItem()));
+                            System.out.println(mva.get(((JComboBox) paramters.get(key).get(i)).getSelectedItem()));
                             para.add(data.get(((JComboBox) paramters.get(key).get(i)).getSelectedItem()));
+                            mvaVal.add(mva.get(((JComboBox) paramters.get(key).get(i)).getSelectedItem()));
                         }
                     }
-                    System.out.println(km);
-                    System.out.println(para);
+                    finaldata[key - 1][2] = String.valueOf(Collections.min(mvaVal));
+
+                    // Calculate the total length
+                    double totallength = 0;
+                    for (Double aDouble : km) {
+                        totallength += aDouble;
+                    }
+                    finaldata[key - 1][3] = ",_f,_f,_f,_f,_f,_f," + String.valueOf(totallength) + ",_f,_f,_f,_f])";
+
                     // Calculate the Length * Type for each part
                     List temp = new ArrayList();
                     int j = 0;
@@ -105,10 +132,10 @@ public class ckts {
                             test.add((Double) list.get(i) * aDouble);
                             //System.out.println(i);
                         }
+
                         j++;
                         temp.add(test);
                     }
-
                     // Store the data in 2D Array to calculate the sum of the parts
                     double[][] num = new double[temp.size()][5];
                     for (int i = 0; i < temp.size(); i++) {
@@ -126,7 +153,34 @@ public class ckts {
                         }
                         parameters.add(sum);
                     }
-                    System.out.println(parameters);
+                    // Write to file
+                    finaldata[key - 1][1] = "[" + String.format("%.6f", parameters.get(0)) + "," + String.format("%.6f", parameters.get(1)) + "," + String.format("%.6f", parameters.get(2)) + ",";
+                    finaldata[key - 1][5] = "[" + String.format("%.6f", parameters.get(3)) + "," + String.format("%.6f", parameters.get(4)) + ",_f,_f,_f,_f,_f])";
+
+                }
+                // Get the Double Circuit check and write to file
+                try {
+                    FileWriter writer = new FileWriter("test2.py", false);
+                    for (Integer key : dckts.keySet()) {
+                        if (dckts.get(key).isSelected()) {
+                            writer.write(finaldata[key - 1][0] + finaldata[key - 1][1] + finaldata[key - 1][2] + finaldata[key - 1][3]);
+                            writer.write("\r\n");
+                            writer.write(finaldata[key - 1][4] + finaldata[key - 1][5]);
+                            writer.write("\r\n");
+                            writer.write(finaldata[key - 1][0].replace(",r\"\"\"1\"\"\",", ",r\"\"\"2\"\"\",") + finaldata[key - 1][1] + finaldata[key - 1][2] + finaldata[key - 1][3]);
+                            writer.write("\r\n");
+                            writer.write(finaldata[key - 1][4].replace(",r\"\"\"1\"\"\",", ",r\"\"\"2\"\"\",") + finaldata[key - 1][5]);
+                            writer.write("\r\n");
+                        } else {
+                            writer.write(finaldata[key - 1][0] + finaldata[key - 1][1] + finaldata[key - 1][2] + finaldata[key - 1][3]);
+                            writer.write("\r\n");
+                            writer.write(finaldata[key - 1][4] + finaldata[key - 1][5]);
+                            writer.write("\r\n");
+                        }
+                    }
+                    writer.close();
+                } catch (IOException ie) {
+                    ie.printStackTrace();
                 }
             }
         });
@@ -183,7 +237,7 @@ public class ckts {
         temp.add(txtFrom);
         temp.add(txtTo);
         fromTxt.put(line, temp);
-        //temp.clear();
+
         dckts.put(line, doubleCkts);
 
         // Parts
